a197db0fe7f938eb153031c110a5b0f42bfbbfa3
Sahil lalotra <sahil.thewarior1990@gmail.com>
2018-03-22 16:21:09 +0530

    data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { GetLocationService } from './get-location.service';
+import { FormsModule }   from '@angular/forms';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
-    HttpClient,
-    HttpHeaders
+    HttpClientModule,
+    FormsModule
   ],
   providers: [GetLocationService],
   bootstrap: [AppComponent]
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {GetLocationService} from '../get-location.service'
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private getLocation: GetLocationService) { }
+
+  public dataApi: any = {};
+  public country: any = [];
+  public currency: any = [];  
+public selected_country = " ";
+public selected_currency = " ";
+
 
   ngOnInit() {
+  this.getData();
+  
   }
+
+  getData() {
+  	var temp = {};
+
+  	this.getLocation.getData().subscribe((data) => {
+  		if(data){
+			  this.dataApi=data;
+			
+  		}
+  	})  
+return this.dataApi;
+  }
+  
+  ngDoCheck(){
+	 var temp=[],temp2=[];
+	 if (this.dataApi){
+	  if(this.dataApi.Continents.length  > 1){
+		for(var i = 0; i < this.dataApi.Continents.length; i++){
+			temp.push(this.dataApi.Continents[i].Countries);
+			for(var j=0; j< temp.length; j++){
+				temp2.push(temp[j])
+				for(var k=0; k < temp[j].length; k++){
+					// console.log("tempj", temp[j][k]);
+					this.country.push(temp[j][k].Name);
+					this.currency.push(temp[j][k].CurrencyId)
+				}
+			}
+		}
+
+	}
+	}
+
+}
+ngOnChanges(){
+	console.log(this.selected_currency);
+	console.log(this.selected_country);
+	
+}
 
 }
